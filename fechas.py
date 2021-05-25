3fb018694f34403025ccadf151b6c48861dee45c
RaulDiMo <84730161+RaulDiMo@users.noreply.github.com>
2021-05-25 19:50:58 +0200

    Add files via upload

---
+++ fechas.py
@@ -0,0 +1,7 @@
+#Usando las funcionalidades de date. Escribir un ejercicio python que imprima el día de hoy, el año y el mes
+#Después que imprima hora y minuto
+
+import datetime
+
+x = datetime.datetime.now()
+print(x)
